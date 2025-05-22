def6d362857aeb11fda00b5ae2aad0f9d26ddb5e
Jefry <jefryjoseph777@gmail.com>
2025-05-22 16:24:13 +0530

    removing unnecessary files

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,8 @@ function Home(){
 return(
 <div className="Home">
 <h1>Home</h1>
+<p>Welcome to TapTravelGo</p>
+<img className="plane" src="https://th.bing.com/th/id/OIP.8LP36rWzcM3dP_iNtSq3egHaEm?cb=iwp2&rs=1&pid=ImgDetMain"></img>
 </div>
 )
 }
