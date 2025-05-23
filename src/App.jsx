6d5fea254ff5f6e4619aa91c2b3feb8ec35eca27
Jefry <jefryjoseph777@gmail.com>
2025-05-23 15:36:05 +0530

    Added contacts page and about us

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import Home from './pages/Home.jsx'
 import Footer from './pages/Footer'
 import Header from './pages/Header'
+import Contact from './pages/Contact.jsx'
+import AboutUs from './pages/AboutUs.jsx'
 
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
 <Header/>
 <Home/>
+<AboutUs/>
+<Contact/>
 <Footer/>
           </>
   )
